585ae7bcfc383f6c7b0fa44ff7b2ba054608a59f
Walheimat <krister.schuchardt@gmail.com>
2024-01-21 19:39:15 +0100

    release: v0.4.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 PACKAGE_NAME=ship-mate
-CURRENT_PACKAGE_VERSION=0.3.4
+CURRENT_PACKAGE_VERSION=0.4.0
 
 include dinghy/emacs-package.mk
--- ship-mate.el
+++ ship-mate.el
@@ -2,7 +2,7 @@
 
 ;; Author: Krister Schuchardt <krister.schuchardt@gmail.com>
 ;; Homepage: https://github.com/Walheimat/ship-mate
-;; Version: 0.3.4
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience
 
